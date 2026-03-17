bd4a259e7e4e42ec93e618c1ed46208c6d3650a5
Lucreciaaaa <lfodouop@gmail.com>
2026-03-17 14:24:36 +0100

    setup src folder

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="p-32 bg-amber-700">
+      <Button>Bonsoir</Button>
+    </div>
+  );
+}
